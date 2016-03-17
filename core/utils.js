e557dd2262a999bdd4eed75b77206781222c45e8
Nestor94Gonzalez <nestor94gonzalez@gmail.com>
2016-03-17 09:15:14 +0000

    new compressed files

--- core/utils.js
+++ core/utils.js
@@ -551,3 +551,21 @@ Blockly.tokenizeInterpolation = function(message) {
   }
   return tokens;
 };
+
+Blockly.readPythonFile = function(file) {
+    var rawFile = new XMLHttpRequest();
+    var code = "";
+    rawFile.open("GET", file, false);
+    rawFile.onreadystatechange = function ()
+    {
+        if(rawFile.readyState === 4)
+        {
+            if(rawFile.status === 200 || rawFile.status == 0)
+            {
+                code = rawFile.responseText;
+            }
+        }
+    }
+    rawFile.send(null);
+    return code;
+};
