30b678fd6b48951e5a2d0bc9ea0f4e40c611d2a2
Nestor94Gonzalez <nestor94gonzalez@gmail.com>
2016-04-11 08:32:38 +0000

    moved all js to scripts folder (brain,spider,rover)

--- generators/python/erle-brain.js
+++ generators/python/erle-brain.js
@@ -30,19 +30,11 @@ goog.require('Blockly.Python');
 Blockly.Python['get_laser'] = function(block) {
 
     var varName = Blockly.Python.valueToCode(block, 'laser', Blockly.Python.ORDER_ATOMIC);
+
     var code = "";
-    code+="import rospy\n"
-    code+="import subprocess\n"
-    code+="import rosnode\n"
-    code+="import rospkg\n"
-    code+="from sensor_msgs.msg import Range\n"
-    code+="ros_nodes = rosnode.get_node_names()\n"
-    code+="if not '/lrm30_node' in ros_nodes:\n"
-    code+="  rospack = rospkg.RosPack()\n"
-    code+="  command = rospack.get_path('lrm30_ros').replace('share', 'lib') + '/lrm30'\n"
-    code+="  process = subprocess.Popen(command, shell=True, stdout=subprocess.PIPE)\n"
-    code+="msg_laser = rospy.wait_for_message('/lrm30_data', Range, timeout=1)\n"
+    code += Blockly.readPythonFile("../blockly/generators/python/scripts/brain/get_laser.py");
     return code + varName + " = msg_laser.range\n"
+
 };
 
 Blockly.Python['take_a_picture'] = function(block) {
@@ -59,86 +51,37 @@ Blockly.Python['take_a_picture'] = function(block) {
 };
 
 Blockly.Python['turn_on_blue_led'] = function(block) {
+
 	var blue_led = block.getFieldValue('BLUE_LED');
-	var code = "";
-	code+="#!/usr/bin/python\n"
-	code+="import rospy\n"
-	code+="import time\n"
-	code+="import rosnode\n"
-	code+="import subprocess\n"
-	code+="from std_msgs.msg import String\n"
-	code+="\n"
-	code+="def talker():\n"
-	code+="  pub = rospy.Publisher('/statusleds', String, queue_size=10)\n"
-	code+="  rate = rospy.Rate(10)\n"
-	code+="  start = time.time()\n"
-	code+="  flag=True #time flag\n"
-	code+="  led='"+blue_led.toString()+"'\n"
-	code+="  if (led == 'TRUE'):\n"
-	code+="    msg = 'blue'\n"
-	code+="  else:\n"
-	code+="    msg = 'blue_off'\n"
-	code+="  while not rospy.is_shutdown() and flag:\n"
-	code+="    sample_time=time.time()\n"
-	code+="    if ((sample_time - start) > 1):\n"
-	code+="      flag=False\n"
-	code+="    pub.publish(msg)\n"
-	code+="    rate.sleep()\n"
-	code+="if __name__ == '__main__':\n"
-	code+="  ros_nodes = rosnode.get_node_names()\n"
-	code+="  if not '/erle_statusleds' in ros_nodes:\n"
-	code+="    command='python /home/erle/spider_ws/src/ros_erle_statusled/scripts/statusleds.py'\n"
-	code+="    process = subprocess.Popen(command, shell=True, stdout=subprocess.PIPE)\n"
-	code+="  talker()\n"	
-	return code;
+
+    var code = "";
+    code += "blue_led = \"" + blue_led.toString() + "\"\n";
+    code += Blockly.readPythonFile("../blockly/generators/python/scripts/brain/blue_led.py");
+    return code;
 
 };
 
 Blockly.Python['turn_on_orange_led'] = function(block) {
+
     var orange_led = block.getFieldValue('ORANGE_LED');
+
     var code = "";
-    code+="#!/usr/bin/python\n"
-    code+="import rospy\n"
-    code+="import time\n"
-    code+="from std_msgs.msg import String\n"
-    code+="\n"
-    code+="def talker():\n"
-    code+="  pub = rospy.Publisher('/statusleds', String, queue_size=10)\n"
-    code+="  rate = rospy.Rate(10)\n"
-    code+="  start = time.time()\n"
-    code+="  flag=True #time flag\n"
-    code+="  led='"+orange_led.toString()+"'\n"
-    code+="  if (led == 'TRUE'):\n"
-    code+="    msg = 'orange'\n"
-    code+="  else:\n"
-    code+="    msg = 'orange_off'\n"
-    code+="  while not rospy.is_shutdown() and flag:\n"
-    code+="    sample_time=time.time()\n"
-    code+="    if ((sample_time - start) > 1):\n"
-    code+="      flag=False\n"
-    code+="    pub.publish(msg)\n"
-    code+="    rate.sleep()\n"
-    code+="if __name__ == '__main__':\n"
-    code+="  talker()\n"
+    code += "orange_led = \"" + orange_led.toString() + "\"\n";
+    code += Blockly.readPythonFile("../blockly/generators/python/scripts/brain/orange_led.py");
     return code;
 
 };
 
 Blockly.Python['calibrate_imu'] = function(block) {
+    
     var code = "";
-	code+="import rosnode\n"
-	code+="import subprocess\n"
-	code+="import time\n"
-	code+="\n"
-	code+="ros_nodes = rosnode.get_node_names()\n"
-	code+="if not '/imu_talker' in ros_nodes:\n"
-	code+="    command='/home/erle/spider_ws/install_isolated/share/ros_erle_imu/imu_talker'\n"
-	code+="    process = subprocess.Popen(command, shell=True, stdout=subprocess.PIPE)\n"
-	code+="    time.sleep(10)\n"
-	return code;
+    code += Blockly.readPythonFile("../blockly/generators/python/scripts/brain/calibrate_imu.py");
+    return code;
+
 };
 
 Blockly.Python['camera_color_location'] = function(block) {
+
     var color = block.getFieldValue('COLOR');
     var varName_location = Blockly.Python.valueToCode(block, 'LOCATION', Blockly.Python.ORDER_ATOMIC);
 
@@ -157,36 +100,17 @@ Blockly.Python['camera_color_location'] = function(block) {
 };
 
 Blockly.Python['start_hokuyo_laser'] = function(block) {
+
     var code = "";
-    code+="import rosnode\n"
-    code+="import subprocess\n"
-    code+="import time\n"
-    code+="import os\n"
-    code+="\n"
-    code+="ros_nodes = rosnode.get_node_names()\n"
-    code+="if not '/urg_node' in ros_nodes:\n"
-    code+="    os.system('ifconfig eth0 192.168.0.2')\n"
-    code+="    ip_add = '192.168.0.10'\n"
-    code+="    command='rosrun urg_node urg_node _ip_address:=' + ip_add\n"
-    code+="    process = subprocess.Popen(command, shell=True, stdout=subprocess.PIPE)\n"
-    //code+="    os.system('roslaunch sick_tim sick_tim571_2050101.launch')\n"
-    code+="    time.sleep(10)\n"
+    code += Blockly.readPythonFile("../blockly/generators/python/scripts/brain/start_hokuyo.py");
     return code;
+
 };
 
 Blockly.Python['start_sick_laser'] = function(block) {
+
     var code = "";
-    code+="import rosnode\n"
-    code+="import subprocess\n"
-    code+="import time\n"
-    code+="import os\n"
-    code+="\n"
-    code+="ros_nodes = rosnode.get_node_names()\n"
-    code+="if not '/robot_state_publisher' in ros_nodes:\n"
-    code+="    os.system('ifconfig eth0 192.168.0.2')\n"
-    code+="    command='roslaunch sick_tim sick_tim571_2050101.launch'\n"
-    code+="    process = subprocess.Popen(command, shell=True, stdout=subprocess.PIPE)\n"
-    //code+="    os.system('roslaunch sick_tim sick_tim571_2050101.launch')\n"
-    code+="    time.sleep(10)\n"
+    code += Blockly.readPythonFile("../blockly/generators/python/scripts/brain/start_sick.py");
     return code;
+
 };
--- generators/python/erle-rover.js
+++ generators/python/erle-rover.js
@@ -29,81 +29,32 @@ goog.require('Blockly.Python');
 
 
 Blockly.Python['rover_mode'] = function(block) {
-  var dropdown_mode = block.getFieldValue('MODE')
-  
-    var code = ""
-    code+="import rospy\n"
-    code+="from mavros_msgs.srv import SetMode\n"
-    //code+="rospy.init_node('rover_mode')\n"
-    code+="rospy.wait_for_service('/mavros/set_mode')\n"
-    code+="try:\n"
-    code+=" change_mode = rospy.ServiceProxy('/mavros/set_mode', SetMode)\n"
-    code+=" mode='"+dropdown_mode.toString()+"'\n"
-    code+=" resp1 = change_mode(0,mode)\n"
-    code+="except rospy.ServiceException as e:\n"
-    code+=' print ("Service call failed: %s" %e\n )'
-    code+="\n"
 
+  var dropdown_mode = block.getFieldValue('MODE')
+  var code = "";
+  code += "dropdown_mode = \"" + dropdown_mode.toString() + "\"\n";
+  code += Blockly.readPythonFile("../blockly/generators/python/scripts/rover/mode.py");
   return code;
+
 };
 
 Blockly.Python['rover_control'] = function(block) {
+
   var dropdown_direction = block.getFieldValue('direction');
   var dropdown_speed = block.getFieldValue('speed');
   var value_seconds = Blockly.Python.valueToCode(block, 'seconds', Blockly.Python.ORDER_ATOMIC);
-  // TODO: Assemble Python into code variable.
-  var code = ""
-  code+="import rospy\n"
-  code+="import time\n"
-  code+="from mavros_msgs.msg import OverrideRCIn\n"
-  code+="from mavros_msgs.srv import SetMode\n"
-  code+="throttle_channel=2\n"
-  code+="steer_channel=0\n"
-   code+="\n"
-  code+="def talker():\n"
-  code+=" pub = rospy.Publisher('mavros/rc/override', OverrideRCIn, queue_size=10)\n"
-  code+=" r = rospy.Rate(10) #10hz\n"
-  code+=" msg = OverrideRCIn()\n"
-  code+=" start = time.time()\n"
-  code+=" speed='"+dropdown_speed.toString()+"'\n"
-  code+=" exec_time="+value_seconds.toString()+"\n"
-  //code+=" msg.channels[steer_channel]=1370\n"
-  code+=" flag=True #time flag\n"
-  code+=" if speed =='SLOW':\n"
-  code+="  msg.channels[throttle_channel]=1558\n"
-  code+=" elif speed =='NORMAL':\n"
-  code+="  msg.channels[throttle_channel]=1565\n"
-  code+=" elif speed == 'FAST':\n"
-  code+="  msg.channels[throttle_channel]=1570\n"
-  code+=" direction='"+dropdown_direction.toString()+"'\n"
-  code+=" if direction =='STRAIGHT':\n"
-  code+="  msg.channels[steer_channel]=1385\n"
-  code+=" elif direction =='RIGHT':\n"
-  code+="  msg.channels[steer_channel]=1450\n"
-  code+=" elif direction == 'LEFT':\n"
-  code+="  msg.channels[steer_channel]=1300\n"
-  code+=" while not rospy.is_shutdown() and flag:\n"
-  code+="  sample_time=time.time()\n"
-  code+="  if ((sample_time - start) > exec_time):\n"
-  code+="   flag=False\n"
-  code+="  rospy.loginfo(msg)\n"
-  code+="  pub.publish(msg)\n"
-  code+="  r.sleep()\n"
-  code+="\n"
-  code+="if __name__ == '__main__':\n"
-  code+=" rospy.wait_for_service('/mavros/set_mode')\n"
-  code+=" change_mode = rospy.ServiceProxy('/mavros/set_mode', SetMode)\n"
-  code+=" resp1 = change_mode(custom_mode='manual')\n"
-  code+=" print (resp1)\n"
-  code+=" if 'True' in str(resp1):\n"
-  code+="  try:\n"
-  code+="   talker()\n"
-  code+="  except rospy.ROSInterruptException: pass\n"
-  code+="\n"
+  
+  var code = "";
+  code += "dropdown_direction = \"" + dropdown_direction.toString() + "\"\n";
+  code += "dropdown_speed = \"" + dropdown_speed.toString() + "\"\n";
+  code += "value_seconds = \"" + value_seconds.toString() + "\"\n";
+  code += Blockly.readPythonFile("../blockly/generators/python/scripts/rover/control.py");
   return code;
+
 };
 
 Blockly.Python['rover_avoid_obstacles'] = function(block) {
+
     var code = "\n";
     code += Blockly.readPythonFile("../blockly/generators/python/scripts/rover/avoid_obstacles.py");
     return code;
