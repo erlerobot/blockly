45708fd39038bc85a94095ebfe58f37b4ae1d175
Nestor94Gonzalez <nestor94gonzalez@gmail.com>
2016-04-26 10:19:49 +0000

    rover mode script fix

--- generators/python/scripts/rover/mode.py
+++ generators/python/scripts/rover/mode.py
@@ -5,6 +5,3 @@ try:
  change_mode = rospy.ServiceProxy('/mavros/set_mode', SetMode)
  mode=dropdown_mode
  resp1 = change_mode(0,mode)
-except rospy.ServiceException as e:
-code+=' print ("Service call failed: %s" %e\n )'
-
