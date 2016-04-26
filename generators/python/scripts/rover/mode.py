f43919e1bb624f925eedeed6bf266ce6cfe365ec
Nestor94Gonzalez <nestor94gonzalez@gmail.com>
2016-04-26 10:22:32 +0000

    rover mode fix2

--- generators/python/scripts/rover/mode.py
+++ generators/python/scripts/rover/mode.py
@@ -1,7 +1,6 @@
 import rospy
 from mavros_msgs.srv import SetMode
 rospy.wait_for_service('/mavros/set_mode')
-try:
- change_mode = rospy.ServiceProxy('/mavros/set_mode', SetMode)
- mode=dropdown_mode
- resp1 = change_mode(0,mode)
+change_mode = rospy.ServiceProxy('/mavros/set_mode', SetMode)
+mode=dropdown_mode
+resp1 = change_mode(0,mode)
